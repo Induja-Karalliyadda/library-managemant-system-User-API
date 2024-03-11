e4e626e21aa4015fe5fa78590e0c3386c7d16236
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-03-11 09:47:07 +0530

    create a User Controller

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,5 +1,17 @@
 package org.example.controller;
 
-public class UserController {
+import org.example.entity.UserEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping
+    public List<UserEntity> getAllUser(){
+
+    }
 }
