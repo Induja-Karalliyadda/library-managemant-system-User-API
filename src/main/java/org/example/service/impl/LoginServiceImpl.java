b5a2166b0211c082dc0f9e037d6f14fa49709090
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-03-18 20:28:48 +0530

    create login service

---
+++ src/main/java/org/example/service/impl/LoginServiceImpl.java
@@ -0,0 +1,11 @@
+package org.example.service.impl;
+
+import org.example.dto.Login;
+import org.example.service.LoginService;
+
+public class LoginServiceImpl implements LoginService {
+    @Override
+    public void insertLoginData(Login login) {
+
+    }
+}
