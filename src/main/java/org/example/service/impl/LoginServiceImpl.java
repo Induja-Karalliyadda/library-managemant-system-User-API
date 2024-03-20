d5f762d3354edd978bf4805654f4f8089a532a23
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-03-20 09:36:20 +0530

    fix bugs

--- src/main/java/org/example/service/impl/LoginServiceImpl.java
+++ src/main/java/org/example/service/impl/LoginServiceImpl.java
@@ -2,7 +2,9 @@ package org.example.service.impl;
 
 import org.example.dto.Login;
 import org.example.service.LoginService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginServiceImpl implements LoginService {
     @Override
     public void insertLoginData(Login login) {
