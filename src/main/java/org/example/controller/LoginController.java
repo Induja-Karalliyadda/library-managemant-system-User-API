4cb2c252577e0e817660fddc2b48c231db5ae6cc
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-03-21 20:16:25 +0530

    login controller update

--- src/main/java/org/example/controller/LoginController.java
+++ src/main/java/org/example/controller/LoginController.java
@@ -1,5 +1,20 @@
 package org.example.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.dto.Login;
+import org.example.repository.LoginRepository;
+import org.example.service.LoginService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/login")
+@RequiredArgsConstructor
 public class LoginController {
+    final LoginService service;
+    @PostMapping("/request-login")
+    public  Boolean validateLogin(@RequestBody Login login){
+       return service.validateLogin(login);
+    }
 
 }
